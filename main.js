703d137f340b75fe787554003f4c381690f46d43
Sungwoo <chltjddn1313@kakao.com>
2022-07-18 06:04:47 +0900

    벌레를 클릭하거나 시간이 0초가 되면 패배팝업/ 당근을 모두 클릭하면 승리팝업/ 리플레이버튼 클릭 시 게임 재시작

--- main.js
+++ main.js
@@ -12,13 +12,30 @@ const fieldRect = field.getBoundingClientRect();
 const time = document.querySelector('.time');
 const count = document.querySelector('.count');
 const alert = document.querySelector('.alert');
+const replay = document.querySelector('.replay');
 const message = document.querySelector('.message');
+
+let time_limit = TIME_LIMIT;
+let bool = true;
+count.innerHTML = CARROT_COUNT;
 
 play.addEventListener('click', ()=> {
   createGame('carrot', CARROT_COUNT, 'img/carrot.png');
   createGame('bug', BUG_COUNT, 'img/bug.png');
   playGame();
+});
 
+replay.addEventListener('click', ()=> {
+  alert.className = 'alert alert__hide';
+  while (field.firstChild != null) {
+    field.removeChild(field.firstChild);
+  }
+  count.innerHTML = CARROT_COUNT;
+  time_limit = TIME_LIMIT;
+  bool = true;
+  createGame('carrot', CARROT_COUNT, 'img/carrot.png');
+  createGame('bug', BUG_COUNT, 'img/bug.png');
+  playGame();
 });
 
 function createGame(className, count, imgPath) {
@@ -42,27 +59,41 @@ function createGame(className, count, imgPath) {
 function randomNumber(min, max) {
   return Math.random() * (max - min) + min;
 }
-let time_limit = TIME_LIMIT;
 
 window.addEventListener('click', (e)=> {
   if (e.target.className == 'carrot') {
-
+    field.removeChild(e.target);
+    count.innerHTML--;
+    if (count.innerHTML == 0) {
+      gameOver(true);
+    }
   } else if (e.target.className == 'bug') {
-    message.innerHTML = 'YOU LOSE!';
-    return alert.className = 'alert';
+    gameOver(false);
   }
 });
 
+function gameOver(bol) {
+  alert.className = 'alert';
+  if (bol) {
+    bool = false;
+    message.innerHTML = 'YOU WIN!';
+  } else {
+    bool = false;
+    message.innerHTML = 'YOU LOSE!';
+
+  }
+}
+
 function playGame() {
   time.innerHTML = time_limit;
-  count.innerHTML = CARROT_COUNT;
   setTimeout(function() {
 
     if(time_limit == 0) {
-      message.innerHTML = 'YOU LOSE!';
-      return alert.className = 'alert';
+      return gameOver(false);
     }
     time_limit--;
-    playGame();
+    if (bool) {
+      playGame();
+    }
   }, 1000);
 }
